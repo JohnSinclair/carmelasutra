92057da30b0eff4ef12bb8e782fff1423d777d27
John Sinclair <jbs96@drexel.edu>
2020-05-30 13:05:38 -0400

    procfile

--- CarmelaSutra/CarmelaSutra/settings.py
+++ CarmelaSutra/CarmelaSutra/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'home',
-    'rules',
-    'uploads',
+    'CarmelaSutra.home',
+    'CarmelaSutra.rules',
+    'CarmelaSutra.uploads',
     'import_export',
 
  
