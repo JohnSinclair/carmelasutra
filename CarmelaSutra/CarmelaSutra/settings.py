724f1d5c62f9605cd38fd91eccc986bd8629a57b
John Sinclair <jbs96@drexel.edu>
2020-05-31 21:25:53 -0400

    procfile

--- CarmelaSutra/CarmelaSutra/settings.py
+++ CarmelaSutra/CarmelaSutra/settings.py
@@ -172,5 +172,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
