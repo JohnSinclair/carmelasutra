5001a97c85ac556eaef530b759acd2ab17c16f57
John Sinclair <jbs96@drexel.edu>
2020-05-31 21:18:59 -0400

    procfile

--- CarmelaSutra/CarmelaSutra/settings.py
+++ CarmelaSutra/CarmelaSutra/settings.py
@@ -78,7 +78,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
