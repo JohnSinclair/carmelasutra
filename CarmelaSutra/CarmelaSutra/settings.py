4aae4d4c19111562817d3c599f4b5291eac5c8b6
John Sinclair <jbs96@drexel.edu>
2020-08-27 07:53:57 -0400

    no message

--- CarmelaSutra/CarmelaSutra/settings.py
+++ CarmelaSutra/CarmelaSutra/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/var/webapps/carmelasutra_static/'
+STATIC_ROOT = '/var/webapps/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),    
 ]
